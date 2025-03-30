c425b4b52106a7803347d80bedd2f4b1d6055912
andrey-blight <kizhinov05@gmail.com>
2025-03-30 16:43:39 +0300

    Поменял вызов создания индекса на subprocess и добавил файл работы с api на фронте

--- API/app/api/v1/endpoints/index.py
+++ API/app/api/v1/endpoints/index.py
@@ -1,4 +1,6 @@
 import asyncio
+import threading
+import time
 
 from fastapi import APIRouter, Depends, UploadFile, HTTPException
 
@@ -7,10 +9,13 @@ from rag import get_files as get_files_from_rag
 from rag import get_indexes as get_indexes_from_rag
 from rag import create_index as create_index_from_rag
 from ocr import ApiOCR
-from app.db.schemas import FilesResponse, IndexesResponse, OcrStatusResponse, IndexRequest
+from app.db.schemas import FilesResponse, IndexesResponse, OcrStatusResponse, IndexRequest, \
+    StatusResponse
 
 router = APIRouter(dependencies=[Depends(validate_user)])
 ocr = ApiOCR()
+
+index_task = False
 
 
 @router.get("/indexes", status_code=200, response_model=IndexesResponse)
@@ -20,8 +25,33 @@ async def get_indexes():
 
 @router.post("/indexes", status_code=200, dependencies=[Depends(validate_admin_user)])
 async def create_index(index_request: IndexRequest):
-    create_index_from_rag(index_request.name, index_request.file_names)
+    global index_task
+
+    if index_task:
+        raise HTTPException(status_code=409, detail="Index still running")
+
+    index_task = True
+
+    def background_task():
+        global index_task
+        try:
+            create_index_from_rag(index_request.name, index_request.file_names)
+            index_task = False
+        except Exception as e:
+            index_task = False
+
+    thread = threading.Thread(target=background_task)
+    thread.start()
+
     return 200
+
+
+@router.get("/indexes/status", status_code=200, dependencies=[Depends(validate_admin_user)])
+async def get_index_status():
+    if index_task:
+        return StatusResponse(status="still running")
+    else:
+        return StatusResponse(status="not running")
 
 
 @router.get("/files", status_code=200, response_model=FilesResponse,
--- frontend/src/api/UploadIndex.js
+++ frontend/src/api/UploadIndex.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import {refreshToken} from "./GetToken";
 
-export const uploadOcr = async (file, navigate) => {
-    const uploadURL = process.env.REACT_APP_API_URL + "/files";
-    const statusUrl = process.env.REACT_APP_API_URL + "/files/status";
-
-    const formData = new FormData();
-    formData.append("file", file);
+export const uploadIndex = async (file_names, name, navigate) => {
+    const uploadURL = process.env.REACT_APP_API_URL + "/indexes";
+    const statusUrl = process.env.REACT_APP_API_URL + "/indexes/status";
 
     const accessToken = localStorage.getItem("access_token");
 
     try {
-        await axios.post(uploadURL, formData, {
+        await axios.post(uploadURL, {
+            'name': name,
+            'file_names': file_names
+        }, {
             headers: {
-                'Authorization': `Bearer ${accessToken}`
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
             }
         });
 
@@ -27,7 +28,7 @@ export const uploadOcr = async (file, navigate) => {
                     }
                 });
 
-                if (statusResponse.status === 200 && statusResponse.data.is_running === false) {
+                if (statusResponse.status === 200 && statusResponse.data.status === "not running") {
                     return true;
                 }
             } catch (statusError) {
@@ -37,7 +38,7 @@ export const uploadOcr = async (file, navigate) => {
     } catch (error) {
         if (error.response?.status === 401) {
             if (await refreshToken(navigate)) {
-                return await uploadOcr(file, navigate);
+                return await uploadIndex(file_names, name, navigate);
             } else {
                 navigate("/login");
             }
