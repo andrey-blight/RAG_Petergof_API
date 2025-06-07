b8f84b5c54547b453b61594f3d3300da404c66ce
andrey-blight <kizhinov05@gmail.com>
2025-06-07 17:35:23 +0300

    Pass user settings to RAG get answer

--- API/app/api/v1/endpoints/rag.py
+++ API/app/api/v1/endpoints/rag.py
@@ -2,8 +2,13 @@ import asyncio
 import threading
 
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from app.core.dependencies import validate_user
+from app.db.models import User, UserSetting
+from app.db.session import get_db
 from rag import get_answer, get_indexes
 from app.db.schemas import TaskResponse, RagQuestion, StatusResponse
 
@@ -15,23 +20,33 @@ tasks = {}
 
 
 @router.post("/answer", status_code=200, response_model=TaskResponse)
-async def get_answer_from_rag(question_schema: RagQuestion):
+async def get_answer_from_rag(question_schema: RagQuestion, user: User = Depends(validate_user),
+                              db: AsyncSession = Depends(get_db)):
     if question_schema.index not in get_indexes():
         raise HTTPException(status_code=404, detail="Index not found")
+
+    result = await db.execute(
+        select(User).options(joinedload(User.settings)).where(User.id == user.id)
+    )
+    user_with_settings = result.scalar_one()
+    user_settings = user_with_settings.settings
 
     task_id = str(time.time())
     tasks[task_id] = False
 
-    def background_task():
-        # try:
-        #     answer = asyncio.run(get_answer(question_schema.index, question_schema.question))
-        #     tasks[task_id] = answer
-        # except Exception as e:
-        #     tasks[task_id] = f"error: {str(e)}"
-        answer = asyncio.run(get_answer(question_schema.index, question_schema.question))
-        tasks[task_id] = answer
+    def background_task(settings: UserSetting):
+        try:
+            answer = asyncio.run(get_answer(index_name=question_schema.index,
+                                            question=question_schema.question,
+                                            temp=settings.temperature,
+                                            faiss_search=settings.count_vector,
+                                            bm_search=settings.count_fulltext,
+                                            prompt=settings.prompt))
+            tasks[task_id] = answer
+        except Exception as e:
+            tasks[task_id] = f"error: {str(e)}"
 
-    thread = threading.Thread(target=background_task)
+    thread = threading.Thread(target=background_task, args=(user_settings,))
     thread.start()
 
     return TaskResponse(task_id=task_id)
