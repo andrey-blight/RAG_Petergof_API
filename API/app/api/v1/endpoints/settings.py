1e55d715bfe2a43d2280bf993b88e4d253d0bed7
andrey-blight <kizhinov05@gmail.com>
2025-06-07 18:01:54 +0300

    Add get settings endpoint

--- API/app/api/v1/endpoints/settings.py
+++ API/app/api/v1/endpoints/settings.py
@@ -7,7 +7,7 @@ from app.core.dependencies import validate_user
 from app.db.models import User
 from app.db.models import apply_update_settings
 from app.db.session import get_db
-from app.db.schemas import  SettingModel
+from app.db.schemas import SettingModel
 
 router = APIRouter(dependencies=[Depends(validate_user)])
 
@@ -22,3 +22,12 @@ async def update_settings(new_settings: SettingModel, user: User = Depends(valid
     user_settings = user_with_settings.settings
 
     return await apply_update_settings(new_settings, user_settings, db)
+
+
+@router.get("/settings", status_code=200, response_model=SettingModel)
+async def get_settings(user: User = Depends(validate_user), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(User).options(joinedload(User.settings)).where(User.id == user.id)
+    )
+    user_with_settings = result.scalar_one()
+    return user_with_settings.settings
